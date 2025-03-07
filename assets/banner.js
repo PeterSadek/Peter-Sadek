1992e5e8e6dfb9d0fbbec417f95cefbc92f2e01c
PeterSadek <Peter_Sadek@outlook.com>
2025-03-07 06:33:02 +0200

    Add animation to the main menu toggle

--- assets/banner.js
+++ assets/banner.js
@@ -1,9 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.querySelector('.menu-toggle')
-    const nav = document.querySelector('.nav')
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const nav = document.querySelector(".nav");
 
-    menuToggle.addEventListener('click', function () {
-        nav.classList.toggle('active')
-        menuToggle.classList.toggle('active')
-    })
-})
+  menuToggle.addEventListener("click", function () {
+    document
+      .querySelector(".banner-head")
+      .classList.toggle("banner-head-mobile");
+    document
+      .querySelector(".banner-body")
+      .classList.toggle("banner-body-mobile");
+    nav.classList.toggle("active");
+    menuToggle.classList.toggle("active");
+  });
+});
